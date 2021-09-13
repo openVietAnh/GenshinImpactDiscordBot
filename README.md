243c6615dab71aea0ed83e2a8e290cd6079f506a
vietanhtran2710 <vietanhtran.uet@gmail.com>
2021-09-13 09:40:13 +0700

    fix bugs and complete data

--- README.md
+++ README.md
@@ -4,12 +4,22 @@ A Discord Helper Bot for a Genshin Impact Server
 # Bot name: Paimon
 
 # Command list
-!introduce: Paimon introduces herself
+## !introduce: Paimon introduces herself
 
-!resin [CURRENT RESIN]: Paimon calculates when the resin will be full.
+## !resin: Paimon calculates when the resin will be full.
+Usage: !resin <current_resin>
+Example: !resin 123
 
-!botngu: Mock the emergency food.
+## !botngu: Mock the emergency food.
 
-!event: Information on current events
+## !event: Information on current events
 
-!ehe: What do you mean EHE?!?
+## !ehe: What do you mean EHE?!?
+
+## !artifact: Generate a random 5-stars artifacts of a certain type
+Usage: !artifact <type> <level>. <Type> must be one of these values: flower, plume, sands, goblet, circlet/
+Example: !artifact flower 20
+
+## !today: Show a list of resources that you can farm today.
+Usage: !today <type>. <Type> must be one of these values: talent, weapon, all.
+Example: !today talent, !today weapon, !today all.
--- domain.py
+++ domain.py
@@ -23,19 +23,19 @@ class Domain(object):
         
         self.weapon = {
             0: {
-                "Decarabian": (""),
-                "Guyun": (""),
-                "Distant Sea": (""),
+                "Decarabian": ("Aquila Favonia", "Royal Longsword", "The Viridescent Hunt", "Snow-Tombed Starsilver", "The Alley Flash", "Favonious Sword", "The Stringless", "Favoinus Codex", "Royal Grimoire", "The Bell"),
+                "Guyun": ("Primordial Jave Winged-Spear", "Summit Shaper", "Lion\'s Roar", "Blackcliff Longsword", "Cresent Pike", "Blackcliff Warbow", "Lithic Blade", "Whileblind", "Rust", "Blackcliff Agate", "Solar Pearl"),
+                "Distant Sea": ("Mistsplitter Reforged", "Amenoma Kageuchi", "Hakushin Ring")
             },
             1: {
-                "Boreal Wolf": (""),
-                "Mist Veiled Elixir": (""),
-                "Narukami": ("")
+                "Boreal Wolf": ("Skyward Harp", "Skyward Blade", "Skyward Pride", "Skyward Atlas", "The Flute", "The Black Sword", " Sacrificial Greatsword", "Sword of Descension", "Deathmatch", "Wine and Song", "The Widsith", "Sacrificial Bow", "Dragonspine Spear"),
+                "Mist Veiled Elixir": ("The Unforged", "Eye of Perception", "Prototype Rancour", "Prototype Crescent", "Dragon's Bane", "Blackcliff Pole", "Royal Spear", "Blackcliff Slasher", "Prototype Amber", "Rainslasher"),
+                "Narukami": ("Thundering Pulse", "Hamayumi", "Katsuragikiri Nagamasa")
             },
             2: {
                 "Dandelion Glaidator": (""),
-                "Aerosiderite": (""),
-                "Mask": ("")
+                "Aerosiderite": ("Memory of Dush", "Vortex Vanquisher", "Iron Sting", "Serpent Spine", "Lithic Spear", "Prototype Starglitter", "Compound Bow", "Mappa Mare", "Prototype Archaic"),
+                "Mask": ("Kitain Cross Spear", "Engulfing Lighting", "The Catch")
             }
         }
         self.weapon[3], self.weapon[4], self.weapon[5] = self.weapon[0], self.weapon[1], self.weapon[2]
@@ -48,10 +48,10 @@ class Domain(object):
         else:
             messages = ["Hôm nay là " + weekDays[day] + " rồi nhà lữ hành, bạn có thể kiếm các loại tài nguyên sau:"]
             if type in ["talent", "book", "all"]:
-                for key, value in self.talent[day]:
-                    messages.append("Sách " + key + ": ", + ", ".join(value))
+                for key, value in self.talent[day].items():
+                    messages.append("Sách **" + key + "**: " + ", ".join(value))
             if type in ["weapon", "all"]:
-                for key, value in self.weapon[day]:
-                    messages.append("Nguyên liệu " + key + ": ", + ", ".join(value))
+                for key, value in self.weapon[day].items():
+                    messages.append("Nguyên liệu **" + key + "**: " + ", ".join(value))
             return messages
 
